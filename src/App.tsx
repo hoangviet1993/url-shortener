c973ff8f0211d06747d0228dc0d2feea1d14dbd5
Tran Quoc Hoang Viet <hoangviet1993@gmail.com>
2020-03-21 18:55:06 -0700

    Add new hero section to app file, add responsive font size for header config, remove unused app css

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { createMuiTheme, MuiThemeProvider } from '@material-ui/core';
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import Hero from './components/Sections/Hero/Hero';
 
 const theme = createMuiTheme({
   typography: {
@@ -9,68 +10,58 @@ const theme = createMuiTheme({
   }
 });
 
+theme.typography.h1 = {
+  fontSize: '3.0rem',
+  [theme.breakpoints.up('md')]: {
+    fontSize: '4.0rem',
+  },
+  [theme.breakpoints.up('lg')]: {
+    fontSize: '5.0rem',
+  },
+};
+
 function App() {
   return (
     <MuiThemeProvider theme={theme}>
       <Navbar />
-      <div className="App">
-        <div className="App-header">
-          <p>
-              More than just shorter links
-            </p>
-          <p>
-            Build your brand’s recognition and get detailed insights 
-            on how your links are performing.
-          </p>
-          <p>
-            More than just shorter links
-          </p>
-          <p>
-            Build your brand’s recognition and get detailed insights 
-            on how your links are performing.
-          </p>
-          <button>
-            Get Started
-          </button>
-          <p>
-            Shorten It section!
-          </p>
-          <p>
-            Advanced Statistics
-          </p>
-          <p>
-            Track how your links are performing across the web with our 
-            advanced statistics dashboard.
-          </p>
-          <p>
-            Brand Recognition
-          </p>
-          <p>
-            Boost your brand recognition with each click. Generic links don’t 
-            mean a thing. Branded links help instil confidence in your content.
-          </p>
-          <p>
-            Detailed Records
-          </p>
-          <p>
-            Gain insights into who is clicking your links. Knowing when and where 
-            people engage with your content helps inform better decisions.
-          </p>
-          <p>
-            Fully Customizable
-          </p>
-          <p>
-            Improve brand awareness and content discoverability through customizable 
-            links, supercharging audience engagement.
-          </p>
-          <p>
-            Boost your links today
-          </p>
-          <button>
-            Get Started
-          </button>
-        </div>
-      </div>
+      <Hero />      
+      <p>
+        Shorten It section!
+      </p>
+      <p>
+        Advanced Statistics
+      </p>
+      <p>
+        Track how your links are performing across the web with our 
+        advanced statistics dashboard.
+      </p>
+      <p>
+        Brand Recognition
+      </p>
+      <p>
+        Boost your brand recognition with each click. Generic links don’t 
+        mean a thing. Branded links help instil confidence in your content.
+      </p>
+      <p>
+        Detailed Records
+      </p>
+      <p>
+        Gain insights into who is clicking your links. Knowing when and where 
+        people engage with your content helps inform better decisions.
+      </p>
+      <p>
+        Fully Customizable
+      </p>
+      <p>
+        Improve brand awareness and content discoverability through customizable 
+        links, supercharging audience engagement.
+      </p>
+      <p>
+        Boost your links today
+      </p>
+      <button>
+        Get Started
+      </button>
     </MuiThemeProvider>
   );
 }
