12e15ccd3801832cc3bf62ac8c040eaaab07293b
Viet Tran Quoc Hoang <hoangviet1993@gmail.com>
2020-04-03 00:12:26 -0700

    Show shortened link component (#18)
    * Add semicolor to import statement
    
    * Add new types file
    
    * Add new relink service file
    
    * Add new constant to constant file
    
    * Add LinkForm functionality to save new link and break up shortened links into subcomponent to process individual link

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -1,6 +1,5 @@
 
 export const urlFieldInputName = 'urlInput';
-
-export interface IFormValues {
-  urlInput: '';
-}
+export const relinkUrl = 'https://rel.ink/api/links/';
+export const shortlyKey = 'shortly'
+export const serializedShortlyKey = JSON.stringify(shortlyKey);
--- src/service/LocalStorageService/index.tsx
+++ src/service/LocalStorageService/index.tsx
@@ -1,30 +1,36 @@
-import axios from 'axios';
 import { FormikHelpers } from 'formik';
 import * as constants from '../../constants';
+import * as types from '../../types';
 
-export function loadHashIDFromLocalStorage(url: string): string | undefined {
+export function isUrlInLocalStorage(url: string): boolean {
   const serializedUrl = JSON.stringify(url);
   const serializedHashID = localStorage.getItem(serializedUrl);
-  return (serializedHashID ? JSON.parse(serializedHashID) : undefined);
+  return (serializedHashID ? true : false);
 }
 
-export function saveUrlToLocalStorage(
-  urlInput: string, formActions: FormikHelpers<constants.IFormValues>): void {
-  const relinkUrl = 'https://rel.ink/api/links/';
-  axios.post(relinkUrl, {
-    url: urlInput
-  })
-    .then((response) => {
-      const serializedUrl = JSON.stringify(response.data.url);
-      const serializedHashID = JSON.stringify(response.data.hashid);
-      try {
-        localStorage.setItem(serializedUrl, serializedHashID);
-      } catch (localStorageError) {
-        formActions.setFieldError(
-          constants.urlFieldInputName, 'Fail to save data to local storage.');
-      }
-    }, (relinkError) => {
-      formActions.setFieldError(
-        constants.urlFieldInputName, relinkError.response.data.url);
-    });
+export function saveToLocalStorage(
+  key: string,
+  value: string,
+  formActions: FormikHelpers<types.IFormValues>): void {
+  const serializedKey = JSON.stringify(key);
+  const serializedValue = JSON.stringify(value);
+  try {
+    localStorage.setItem(serializedKey, serializedValue);
+  } catch (localStorageError) {
+    formActions.setFieldError(
+      constants.urlFieldInputName, 'Fail to save data to local storage.');
+  }
+};
+
+export function saveNewLinkToLocalStorage(
+  newLink: types.ILink,
+  formActions: FormikHelpers<types.IFormValues>): void {
+  let shortenedLinks = [];
+  const localStorageLinks = localStorage.getItem(
+    constants.serializedShortlyKey);
+  if (localStorageLinks) {
+    shortenedLinks = JSON.parse(localStorageLinks);
+  }
+  shortenedLinks.push(newLink);
+  saveToLocalStorage(constants.shortlyKey, shortenedLinks, formActions);
 }
