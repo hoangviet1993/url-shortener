74e44c34ac0669d30993aecab73d187cfcfd296b
Tran Quoc Hoang Viet <hoangviet1993@gmail.com>
2020-04-05 21:34:04 -0700

    Add copy to clipboard button

--- src/types/index.tsx
+++ src/types/index.tsx
@@ -1,11 +1,16 @@
 
-type SetStringFunc = (inputList: string | null) => void;
+type SetShortlyLinks = (shortlyLinks: string | null) => void;
+
+type SetHashIDFunc = (hashID: string) => void;
+
+type SetIsCopied = (isCopied: boolean) => void;
 
 export interface IFormValues {
   urlInput: string;
 }
 
 export interface IShortenedLinkProp {
+  children: React.ReactNode;
   hashID: string;
   url: string;
 }
@@ -15,10 +20,17 @@ export interface IShortenedLinksProp {
 }
 
 export interface ILinkFormProp {
-  setshortlyLinks: SetStringFunc;
+  setshortlyLinks: SetShortlyLinks;
 }
 
 export interface ILink {
-  hashID: string,
-  url: string,
+  hashID: string;
+  url: string;
+}
+
+export interface ICopyToClipboardButtonProp {
+  hashID: string;
+  isCopied: boolean;
+  setIsCopied: SetIsCopied;
+  setShowCopiedButtonHash: SetHashIDFunc;
 }
